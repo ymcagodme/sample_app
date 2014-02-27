d5ba951731c99d9ce462f5338aab4070f6ef35af
ymcagodme <ymcagodme@gmail.com>
2014-02-26 21:29:26 -0800

    merge

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,16 @@ describe User do
 
   it { should be_valid }
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExaMPLE.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email;
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
@@ -70,7 +80,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[foo@bar,com user_at_foo.org example@foo.com@yahoo.com foo@bar+com]
+      addresses = %w[foo@bar,com user_at_foo.org example@foo.com@yahoo.com foo@bar+com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
