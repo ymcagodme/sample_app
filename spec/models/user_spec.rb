275628fd611921a3c1105e05b09c53a39a927fd7
ymcagodme <ymcagodme@gmail.com>
2014-02-26 18:25:47 -0800

    merge

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,4 +25,34 @@ describe User do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
   end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[foo@bar,com user_at_foo.org example@foo.com@yahoo.com foo@bar+com]
+      addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(user).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[foo@bar.COM A.US_ER@FOO.a.c fist@huang.jp a+B@yahoo.com]
+      addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when the email address is already taken" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+
+    it { should_not be_valid }
+  end
 end
