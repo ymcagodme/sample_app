ddc38193ff8a9ea48367d1ba844ec848473129a1
ymcagodme <ymcagodme@gmail.com>
2014-02-26 20:07:30 -0800

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,56 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.ocm") }
+  before do
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar"  , password_confirmation: "foobar")
+  end
 
   subject{ @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " "  , password_confirmation: " ")
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
 
   describe "when name is not present" do
     before { @user.name = " "}
@@ -31,7 +73,7 @@ describe User do
       addresses = %w[foo@bar,com user_at_foo.org example@foo.com@yahoo.com foo@bar+com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
-        expect(user).not_to be_valid
+        expect(@user).not_to be_valid
       end
     end
   end
